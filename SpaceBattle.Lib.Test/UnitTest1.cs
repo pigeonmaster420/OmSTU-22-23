a5d1f2e22d7353c24df02bca0032817f91d32fd2
Golub <lifostdafox@gmail.com>
2022-10-11 14:35:09 +0600

    IRotatable works, write tests for it

--- SpaceBattle.Lib.Test/UnitTest1.cs
+++ SpaceBattle.Lib.Test/UnitTest1.cs
@@ -50,3 +50,34 @@ public class MoveTests
         Assert.Throws<Exception>(() => move.execute());
     }
 }
+public class RotateTests
+{
+    [Fact]
+    public void PositiveRotateCommand()
+    {
+        var mock = new Mock<IRotatable>();
+        mock.SetupProperty<float>(m => m.angle, 0);
+        mock.SetupGet<float>(m => m.rotatespd).Returns(30);
+        RotateCommand rotate = new RotateCommand(mock.Object);
+
+        rotate.execute();
+
+        Assert.Equal(30,mock.Object.angle);
+    }
+    [Fact]
+    public void NegativeCantSetAngle()
+    {
+        var mock = new Mock<IRotatable>();
+        mock.SetupProperty<float>(m => m.angle, 0);
+        mock.SetupGet<float>(m => m.rotatespd).Returns(30);
+        mock.SetupSet(m => m.angle = It.IsAny<float>()).Throws<Exception>();
+        RotateCommand rotate = new RotateCommand(mock.Object);
+
+        Assert.Throws<Exception>(() => rotate.execute());
+    }
+    [Fact]
+    public void NegativeCantGetAngle()
+    {
+        var mock = new Mock<IRotatable>();
+    }
+}
