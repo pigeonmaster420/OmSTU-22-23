da1386639de51ceff3b3e646e5ec76d713578363
Vlad Golub <lifostdafox@gmail.com>
2023-01-20 00:18:25 +0600

    reused interfaces from previous laboratory works

---
+++ SpaceBattle.lib/Move/IMovable.cs
@@ -0,0 +1,15 @@
+using System.Numerics;
+namespace SpaceBattle.lib;
+
+public interface IMovable
+{
+    Vector2 pos
+    {
+        get;
+        set;
+    }
+    Vector2 spd
+    {
+        get;
+    }
+}
